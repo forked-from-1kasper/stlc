86feb3ecc8959c717991ada6ee33e1c642eb7de9
Siegmentation Fault <siegmentationfault@yandex.ru>
2021-02-16 18:28:35 +0700

    evaluator

--- stlc.ml
+++ stlc.ml
@@ -20,6 +20,37 @@ let replicate size value =
 let const idx value =
   replicate idx "⊢-rec" @ [value]
 
+let value : int ref = ref 0
+let gensym () =
+  value := !value + 1;
+  Printf.sprintf "?%d" !value
+
+let conv =
+  let rec func map = function
+    | EVar x when SM.mem x map -> EVar (SM.find x map)
+    | EApp (f, x)              -> EApp (func map f, func map x)
+    | ELam (x, t, p)           ->
+      let y = gensym () in
+      ELam (y, t, func (SM.add x y map) p)
+    | tau                      -> tau in
+  func SM.empty
+
+let rec subst x v = function
+  | EVar y when x = y -> v
+  | EApp (f, y)       -> EApp (subst x v f, subst x v y)
+  | ELam (y, t, u)    -> ELam (y, t, subst x v u)
+  | tau               -> tau
+
+let rec eval = function
+  | EApp (f, x)    ->
+    let y = eval x in
+    begin match eval f with
+    | ELam (z, _, p) -> subst z y (eval p)
+    | g              -> EApp (g, y)
+    end
+  | ELam (x, t, p) -> ELam (x, t, eval p)
+  | tau            -> tau
+
 let rec infer vars idx = function
   | EZero          -> const idx "0-def"
   | ESucc          -> const idx "succ-def"
@@ -56,8 +87,18 @@ let check name e t : string =
 
 let term1 = EApp (EApp (EApp (EIte, ETrue), EApp (ESucc, EZero)), EZero)
 let term2 = ELam ("x", TNat, ELam ("y", TNat, EApp (ESucc, EApp (ESucc, EVar "x"))))
+let term3 = EApp (EApp (term2, EZero), EApp (ESucc, EZero))
 
 let () =
+  term3
+  |> showExp
+  |> Printf.printf "%s\n";
+
+  conv term3
+  |> eval
+  |> showExp
+  |> Printf.printf "%s\n";
+
   check "term1" term1 TNat
   |> Printf.printf "%s\n";
 
